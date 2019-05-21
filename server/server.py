feb0f1edde3ff1596366b91f9d93eec3abb12263
VitorRedfox <vitor.pereira@giganti.com.br>
2019-05-21 14:27:51 -0300

    init api de consumo

---
+++ server/server.py
@@ -0,0 +1,25 @@
+from flask import Flask, jsonify, render_template, redirect, request, url_for
+import requests
+import json
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def contatos():
+    r = requests.get('http://localhost:5000/')
+    cont = json.loads(r.text)
+
+    if 'ctt' in request.args:
+        lol = json.loads(request.args['ctt'])
+        cont.append(lol)
+
+    return render_template('contatos.html', contatos=cont)
+
+@app.route('/salvar', methods=['POST'])
+def salvar():
+    contato = {'Nome': request.form['Nome'], 'Numero': request.form['Numero']}
+    c = jsonify(contato)
+    return redirect(url_for('.contatos', ctt=c.data))
+
+if __name__ == '__main__':
+    app.run(host='localhost', port=5001)
