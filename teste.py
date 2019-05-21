20a5509fb7f53f7f8e487595cbd5280fe0804e8a
Victor Cardoso <victor_92santos@hotmail.com>
2019-05-21 14:20:29 -0300

    testando retorno de id depois da insercao

--- teste.py
+++ teste.py
@@ -4,13 +4,14 @@ from dao.contatoDao import ContatoDao
 from model.endereco import Endereco
 
 db = Database()
+dao = ContatoDao()
 
 db.cria()
+
+contato = Contato('victor','123','456','teste@teste',4)
+
+#print(contato.getAll())
 '''
-contato = Contato('victor')
-
-print(contato.getAll())'''
-
 try:
     conn = Database().conexao()
 
@@ -19,10 +20,17 @@ try:
         (cep, logradouro, bairro, numero, complemento, localidade, uf)\
         values(?,?,?,?,?,?,?)",('777','rua 777', 'bairro', '77', 'casa 7', 'sao paulo', 'SP'))
     
+    cur.execute("INSERT INTO contato (nome, celular, telefone, email, idEndereco)\
+                values('victor', '123', '456', 'vitor@vitor',55)")
     conn.commit()
     cur.close()
     conn.close()
 except Exception as e:
     print(e)
 
-end = Endereco('07124000', )
+'''
+#print(contato.getNome())
+print(dao.insere(contato))
+end = Endereco(contato)
+
+#print(end.contato.getAll())
