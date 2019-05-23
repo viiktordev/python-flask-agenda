3171b926bb47a060434c053309dd1fd36ab0301d
VitorRedfox <vitor.pereira@giganti.com.br>
2019-05-23 14:32:47 -0300

    funcao retorna object

--- model/contato.py
+++ model/contato.py
@@ -1,20 +1,25 @@
 class Contato():
-    def __init__(self, nome, celular = '', telefone = '', email = '', endereco = ''):
-        self.nome = nome
+    def __init__(self, id, nome, celular = '', telefone = '', email = '', endereco = ''):
+        self.__id = id
+        self.__nome = nome
         self.__celular = celular
         self.__telefone = telefone
         self.__email = email
         self.__endereco = endereco
 
     def getAll(self):
-        return({'nome':self.nome,
+        return({'id':self.__id,
+            'nome':self.__nome,
             'celular':self.__celular,
             'telefone':self.__telefone,
             'email':self.__email,
             'endereco':self.__endereco})
             
+    def getId(self):
+        return self.__id
+
     def getNome(self):
-        return self.nome
+        return self.__nome
 
     def getCelular(self):
         return self.__celular
@@ -27,3 +32,13 @@ class Contato():
     
     def getEndereco(self):
         return self.__endereco
+
+    def __dict__(self):
+        d = ditc()
+        d["id"] = self.__id
+        d["nome"] = self.__nome
+        d["celular"] = self.__celular
+        d["telefone"] = self.__telefone
+        d["email"] = self.__email
+        d["endereco"] = self.__endereco
+        return d
