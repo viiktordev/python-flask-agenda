2977ca09f0a1a3551e50cf9f2040e7aa4a3e2822
VitorRedfox <vitor.pereira@giganti.com.br>
2019-05-23 13:38:31 -0300

    front listar, criar, editar contato

--- server/server.py
+++ server/server.py
@@ -6,14 +6,26 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def contatos():
+    '''
     r = requests.get('http://localhost:5000/')
     cont = json.loads(r.text)
 
     if 'ctt' in request.args:
         lol = json.loads(request.args['ctt'])
         cont.append(lol)
+    '''
+    return render_template('contatos.html')
 
-    return render_template('contatos.html', contatos=cont)
+@app.route('/criar', methods=['GET'])
+def criarContato():
+
+    return render_template('criar.html')
+
+@app.route('/editar/<int:id>', methods=['GET'])
+def editarContato(id):
+
+    return render_template('editar.html', id=id)
+
 
 @app.route('/salvar', methods=['POST'])
 def salvar():
